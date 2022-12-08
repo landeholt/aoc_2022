073205b677a2196dc8561b44140cae5f4b883f68
John Landeholt <johnlan@kth.se>
2022-12-08 14:03:45 +0100

    cleanup of day 8

--- aoc_2022/day08/core.py
+++ aoc_2022/day08/core.py
@@ -1,4 +1,3 @@
-import math
 from typing import Generator, List, Tuple
 
 Grid = List[List[int]]
@@ -9,48 +8,32 @@ def shape(grid: Grid):
 
 
 def path(ra: range, fn) -> Generator[Tuple[int, int], None, None]:
-    for k in ra:
-        r = fn(k)
-        if r[0] >= 0 and r[1] >= 0:
-            yield r
+    yield from (fn(k) for k in ra)
+
+
+def filter_path(path: Generator[Tuple[int, int], None, None], i, j):
+    yield from (p for p in path if p != (i, j) and p[0] >= 0 and p[1] >= 0)
 
 
 def ileft(grid: Grid, i, j):
-    _, y = shape(grid)
-    return filter(lambda p: p != (i, j), path(range(1, y), lambda k: (i, j-k)))
+    return path(range(1, len(grid[0])), lambda k: (i, j-k))
 
 
 def iright(grid: Grid, i, j):
-    _, y = shape(grid)
-    return filter(lambda p: p != (i, j), path(range(y-j), lambda k: (i, j+k)))
+    return path(range(len(grid[0])-j), lambda k: (i, j+k))
 
 
 def iup(grid: Grid, i, j):
-    x, _ = shape(grid)
-    return filter(lambda p: p != (i, j), path(range(1, x), lambda k: (i-k, j)))
+    return path(range(1, len(grid)), lambda k: (i-k, j))
 
 
 def idown(grid: Grid, i, j):
-    x, _ = shape(grid)
-    return filter(lambda p: p != (i, j), path(range(x-i), lambda k: (i+k, j)))
+    return path(range(len(grid)-i), lambda k: (i+k, j))
 
 
 def isvisible(grid: Grid, i, j):
     paths = [iup, idown, ileft, iright]
-
-    for path in paths:
-        if all(grid[i][j] > grid[pi][pj] for pi, pj in path(grid, i, j)):
-            return True
-    return False
-
-
-def isedge(grid: Grid, i, j):
-    x, y = shape(grid)
-    return x-1 == i or y-1 == j or i == 0 or j == 0
-
-
-def distance(pi, pj, qi, qj):
-    return int(math.sqrt(math.pow((qi - pi), 2) + math.pow((qj - pj), 2)))
+    return any(all(grid[i][j] > grid[pi][pj] for pi, pj in filter_path(path(grid, i, j), i, j)) for path in paths)
 
 
 def get_scenic_score(grid: Grid, i, j):
@@ -58,32 +41,21 @@ def get_scenic_score(grid: Grid, i, j):
     score = 1
     for path in paths:
         t = 0
-        for pi, pj in path(grid, i, j):
+        for pi, pj in filter_path(path(grid, i, j), i, j):
             t += 1
             if grid[pi][pj] >= grid[i][j]:
                 break
         score *= t
-    
-    if score == 1:
-        return 0
-    else:
-        return score
+    return score if score > 1 else 0
 
 
 def first(data: str):
     grid = [list(map(int, list(d))) for d in data.splitlines()]
     x, y = shape(grid)
-    total = 0
-    for i in range(1, x-1):
-        total += sum(isvisible(grid, i, j) for j in range(1, y-1))
-    return x * 2 + (y-2)*2 + total
+    return x * 2 + (y-2) * 2 + sum(isvisible(grid, i, j) for j in range(1, y-1) for i in range(1, x-1))
 
 
 def second(data: str):
     grid = [list(map(int, list(d))) for d in data.splitlines()]
     x, y = shape(grid)
-    scores = []
-    for i in range(x):
-        for j in range(y):
-            scores.append(get_scenic_score(grid, i, j))
-    return max(scores)
+    return max(get_scenic_score(grid, i, j) for j in range(y) for i in range(x))
